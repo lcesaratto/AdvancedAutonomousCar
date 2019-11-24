d5ce58bac6eadad1d1d529aabefa5b061d69124a
lcesaratto <lisandrocesaratto95@gmail.com>
2019-11-24 18:55:30 -0300

    agregamos distancia entre lineas rectas

--- index.py
+++ index.py
@@ -149,7 +149,7 @@ def qr_reader():
         print("[INFO] found {} barcode {}".format(barcodeType, barcodeData))
     cv2.imwrite("new_img.jpg", img)
 
-def line_keeping_grid():
+def line_keeping_grid_v1():
     # Create a VideoCapture object and read from input file
     # If the input is the camera, pass 0 instead of the video file name
     cap = cv2.VideoCapture('Videos/20191012_213614.mp4')#('Videos/WhatsApp Video 2019-10-12 at 6.19.29 PM(2).mp4')
@@ -333,6 +333,220 @@ def line_keeping_grid():
             # Modify the image to include the grid
             frameResulting[:,::dy,:] = grid_color
             frameResulting[::dx,:,:] = grid_color
+
+            cv2.line(frameResulting,(0,140),(640,140),(255,255,255),2)
+            # Display the resulting frame
+            cv2.imshow('frameResulting', frameResulting)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):  # 25fps
+                break
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+def line_keeping_grid_v2():
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
+
+    cap = cv2.VideoCapture('Videos/20191012_213614.mp4')#('Videos/WhatsApp Video 2019-10-12 at 6.19.29 PM(2).mp4')
+
+    lower_black = np.array([0, 0, 0]) #108 6 17     40 16 37
+    upper_black = np.array([150, 75, 255]) #HSV 255, 255, 90 #HLS[150, 75, 255]
+
+    # Check if camera opened successfully
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+
+    # Read until video is completed
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        if ret:
+            right_points_up = np.array([0, 0])
+            left_points_up = np.array([0, 0])
+
+            frame = cv2.flip(frame, flipCode=-1)
+            #Defino parametros HLS o HSV para detectar solo lineas negras 
+            lower_black = np.array([0, 0, 0]) #108 6 17     40 16 37
+            upper_black = np.array([150, 75, 255]) #HSV 255, 255, 90 #HLS[150, 75, 255]
+            #Divido la imagen en 2 frames distintos (derecha e izquierda) para detectar una sola linea por frame
+            # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH )
+            # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )
+            fps =  cap.get(cv2.CAP_PROP_FPS)
+            width = cap.get(cv2.CAP_PROP_FRAME_WIDTH )
+            height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )
+            frame=cv2.flip(frame, flipCode=-1)
+            frame = frame[0:int(height*0.5),0:int(width)]
+
+
+            # print(int(width))    #640 --->32
+            # print(int(height*0.5))   #240 --->12
+            # 32*12= 384 bloques
+            column=0
+            row=0
+            frameResulting=frame
+
+            #Aplico filtro de color con los parametros ya definidos
+            # hsv_right = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS) #BRG2HLS
+            hsv_right = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #BRG2HLS
+            # cv2.imshow('framefilterHSV', hsv_right)
+            mask_right = cv2.inRange(frame, lower_black, upper_black)
+            res_right = cv2.bitwise_and(frame, frame, mask=mask_right)
+            # cv2.imshow('framefilter', res_right)
+
+            #Aplico filtro pasa bajos y deteccion de lineas por Canny
+            '''
+            kernel = np.ones((3,3), np.uint8)
+            frame_e = cv2.erode(frame, kernel, iterations=1)
+            gray_right = cv2.cvtColor(frame_e, cv2.COLOR_BGR2GRAY) 
+            (thresh, bw_right) = cv2.threshold(gray_right, 40, 255, cv2.THRESH_BINARY)
+            #dif_gray_right = cv2.GaussianBlur(bw_right, (1, 1), 0) #(mask, (5, 5), 0)
+            cv2.imshow('framefilterdif', frame_e)
+            canny_right = cv2.Canny(bw_right, 25, 175) # 25, 175)
+            cv2.imshow('framefilter', canny_right)
+            '''
+            #Umbral Dinamico
+            kernel = np.ones((3,3), np.uint8)
+            frame_e = cv2.erode(frame, kernel, iterations=1)
+            gray = cv2.cvtColor(frame_e, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (3, 3), 0)
+            gray = cv2.medianBlur(gray, 5)
+            dst2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 5)
+            cv2.imshow('framefilterdif', dst2)
+            dif_gray_right = cv2.GaussianBlur(dst2, (11, 11), 0) #(mask, (5, 5), 0)
+            #cv2.imshow('framefilterdif', frame_e)
+            canny_right = cv2.Canny(dif_gray_right, 25, 175) # 25, 175)
+            kernel = np.ones((7,7), np.uint8)
+            canny_right = cv2.morphologyEx(canny_right, cv2.MORPH_CLOSE, kernel)
+            kernel = np.ones((3,3), np.uint8)
+            canny_right = cv2.erode(canny_right, kernel, iterations=1)
+            cv2.imshow('framefilter', canny_right)
+            
+            # list = []
+            '''Recorre de abajo para arriba, de izquierda a derecha'''
+            while row < 6:
+                if column < 16:
+
+                    cannyCut=canny_right[(200-row*40):(240-row*40),(40*column):(40+40*column)]
+                    frameCut=frame[(200-row*40):(240-row*40),(40*column):(40+40*column)]
+
+                    if row == 2:
+                        '''Aca procesamos cada frameCut'''
+                        try:
+
+                            # frameCut = canny_right
+                            ## Aplico Transformada de Hough
+                            lines_right = cv2.HoughLinesP(cannyCut, 1, np.pi / 180, 10, minLineLength=0, maxLineGap=1) #(canny, 1, np.pi / 180, 30, minLineLength=15, maxLineGap=150)
+                            # Draw lines on the image
+                            if lines_right is not None:
+                                cant_lineas=len(lines_right)
+                                #print(cant_lineas)
+                                
+                                if cant_lineas>100: #11
+                                    #print('Se detecto una curva')
+                                    frameCut[:,:,2] = frameCut[:,:,2] + 50
+                                    #frameCut[:,:,0] = 0
+                                else:
+                                    #print('Se detecto una linea')
+                                
+                                    x1prom=0
+                                    x2prom=0
+                                    y1prom=0
+                                    y2prom=0
+                                    cant=0
+
+                                    righty_sum=0
+                                    lefty_sum=0
+                                    counter=0
+                                    x1m=640
+                                    x1M=0
+                                    columnm=16
+                                    for line in lines_right: #for line in lines:
+                                        x1, y1, x2, y2 = line[0]
+                                        
+                                        if(x1>x1M):
+                                            x1M=x1
+                                            right_points_up = [x1+40*column, y1+40*(5-row)]
+                                            right_points_down = [x2, y2]
+                                        if(column<7):
+                                            # print(x1+40*column)
+                                            left_points_up = [x1+40*column, y1+40*(5-row)]
+                                            left_points_down = [x2, y2]
+                                        #x1m=x1
+                                        #right_points = [(x1,y1), (x2,y2)]  
+                                        cv2.line(frameCut,(x1,y1),(x2,y2),(0,0,255),2)
+                                        #cv2.line(frameCut,(frameCut.shape[1]-1,x1),(0,y1),255,2)
+                                    '''
+                                    [vx,vy,x,y] = cv2.fitLine(np.array(right_points, dtype=np.int32), cv2.DIST_L2, 0, 0.01, 0.01)
+                                    
+                                    # Now find two extreme points on the line to draw line
+                                    lefty = int((-x*vy/vx) + y)
+                                    righty = int(((frameCut.shape[1]-x)*vy/vx)+y)
+
+                                        #Finally draw the line
+                                    
+                                    if (abs(vy/vx) > 1) & (abs(vy/vx) < 30) :
+                                        righty_sum+=righty
+                                        lefty_sum+=lefty
+                                        counter+=1
+                                    '''
+                                    '''
+                                    if (counter!=0) & (righty_sum!=0) & (lefty_sum!=0):
+                                        righty=righty_sum//counter
+                                        lefty=lefty_sum//counter
+                                        cv2.line(frameCut,(frameCut.shape[1]-1,righty),(0,lefty),255,2)
+                                    
+                                    cv2.line(frameCut,(frameCut.shape[1]-1,righty),(0,lefty),255,2)
+                                        # print(frameCut.shape[1]-1)
+                                        '''
+                                    frameCut[:,:,0] = frameCut[:,:,0]+50
+                            else:
+                                frameCut[:,:,1] = frameCut[:,:,1] + 50        
+
+                        except Exception as e:
+                            print(e)
+
+                        '''Aca terminamos de procesar cada frameCut'''
+
+                    for x in range(40): #filas
+                        for j in range(40): #columnas
+                            # print(frameCut[x][j])
+                            frameResulting[200-row*40+x][0+40*column+j] = frameCut[x][j]
+                    column+=1
+                    # list.append(frameCut)
+                elif column == 16:
+                    column=0
+                    row+=1
+            '''     
+            if (frame == frameResulting).all(): #Esto es para verificar que el frame original sea igual al reconstruido
+                print ("Excelente!")
+                '''
+            # frameCut=frame[(1):(2),(1):(2)]
+            # print("hola0", frame[1][1])
+            # print("hola00", frame[239][639])
+            # print("hola1", frameCut[0][0])
+            # # print("hola2", list[383])
+            # cv2.imshow('frameCut', frameCut)
+
+            # Grid lines at these intervals (in pixels)
+            # dx and dy can be different
+            dx, dy = 40,40
+            # Custom (rgb) grid color
+            grid_color = [255,0,0]# [0,0,0]
+            # Modify the image to include the grid
+            frameResulting[:,::dy,:] = grid_color
+            frameResulting[::dx,:,:] = grid_color
+
+            #cv2.line(frameResulting,(0,140),(640,140),(255,255,255),2)
+            cv2.line(frameResulting,(left_points_up[0],140),(right_points_up[0],140),(255,255,255),2)
             # Display the resulting frame
             cv2.imshow('frameResulting', frameResulting)
 
@@ -353,4 +567,4 @@ if __name__ == "__main__":
     # line_keeping()
     # qr_reader()
     # ML_test()
-    line_keeping_grid()
+    line_keeping_grid_v2()
