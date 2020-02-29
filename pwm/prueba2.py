6a20d7c9353b8e6aea27fa4c669dbc0dfb786dab
lcesaratto <lisandrocesaratto@hotmail.com>
2020-02-29 20:30:25 -0300

    agregado de archivos de pwm

---
+++ pwm/prueba2.py
@@ -0,0 +1,6 @@
+import wiringpi as wpi
+
+wpi.wiringPiSetup()
+wpi.pinMode(2,1)
+
+wpi.digitalWrite(2,1)
---
+++ pwm/prueba3.py
@@ -0,0 +1,6 @@
+import wiringpi as wpi
+
+wpi.wiringPiSetup()
+wpi.pinMode(2,0)
+
+wpi.digitalWrite(2,0)
