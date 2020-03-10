e6aca7b02491a73638332322b8846d846d8bf75b
lcesaratto <lisandrocesaratto95@gmail.com>
2020-03-10 19:12:38 -0300

    sacamos clase de videocamera

--- deteccionLineas.py
+++ deteccionLineas.py
@@ -8,42 +8,6 @@ from threading import Thread
 import copy
 from controlPWM import *
 
-'''
-class VideoCamera(object):
-    #320*240 original
-    def __init__(self):
-        self.cap = cv2.VideoCapture(0)
-        # initialize the frame and the variable used to indicate
-        # if the thread should be stopped
-        self.frame = None
-        self.stopped = False
-
-    def start(self):
-        # start the thread to read frames from the video stream
-        Thread(target=self.update, args=()).start()
-        return self
-
-    def update(self):
-        # keep looping infinitely until the thread is stopped
-        while self.cap.isOpened():
-            # grab the frame from the stream and clear the stream in
-            # preparation for the next frame
-            self.ret, self.frame = self.cap.read()
-            # if the thread indicator variable is set, stop the thread
-            # and resource camera resources
-            if self.stopped:
-                self.cap.release()
-                return
-
-    def read(self):
-        # return the frame most recently read
-        return self.ret, self.frame
-
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
-'''
-
 class VehiculoAutonomo (object):
     def __init__(self):
         self.cap = self._abrirCamara()
@@ -471,6 +435,7 @@ class VehiculoAutonomo (object):
             forward(self.miPwm, 1000)
         if abs(distancia_al_centro) != 320:
             self.ultima_distancia = distancia_al_centro
+    
     def _girarLineaPunteada(self):
         self.columnasDeseadas = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
         self.filasDeseadas = [0,1,2,3,4,5]
