3058303bc7508e6f3d69dc040f05296dca93695f
lcesaratto <lisandrocesaratto95@gmail.com>
2020-05-03 15:49:19 -0300

    detectando pendiente en linea vertical verde

--- probandoLineaVerdeImg.py
+++ probandoLineaVerdeImg.py
@@ -3,7 +3,7 @@ import numpy as np
 import copy
 import statistics
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('imgverde.jpg')
 width = 640
 i = 1
 while cap.isOpened():
@@ -19,18 +19,31 @@ while cap.isOpened():
         hsv_green = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask_green = cv2.inRange(hsv_green, lower_green, upper_green)
         y, x = np.where(mask_green == 255)
+        # print(y)
+        y += 320
+        # cv2.imshow('imagenverde', mask_green)
+        print(len(x))
         try:
             if len(x) < 100:
                 x_mid = 0
             else:
                 x_mid= statistics.median(x)
+                y_mid = statistics.median(y)
         except:
             x_mid = 0
         x_mid_int=int(round(x_mid))
+        print(x_mid)
+        print(y_mid)
         ubicacion_punto_verde = x_mid_int
         distancia_al_centro = 320 - ubicacion_punto_verde
-        if x.size > 50 and distancia_al_centro < 320:    
-            m,b = np.polyfit(x,y,1)
+        if x.size > 50 and distancia_al_centro < 320:
+            m,b = np.polyfit(y,x,1)
+        print(m,b)
+        m2 = 1/m
+        b2 = -b/m
+        print(m2,b2)
+        print(int((320-b2)/m2))
+        print(int((480-b2)/m2))
         
         # cv2.line(mask_green,(ubicacion_punto_verde,0),(ubicacion_punto_verde,480),(255,255,255), 2)
 
@@ -39,14 +52,14 @@ while cap.isOpened():
         frameAMostrar[:,:,1] = mask_green
         frameAMostrar[:,:,2] = mask_green
         cv2.line(frameAMostrar,(ubicacion_punto_verde,0),(ubicacion_punto_verde,480),(0,255,0), 3)
-        cv2.line(frameCamara, (int((320-b)/m),320), (int((480-b)/m),480), (255,0,0), 3)
+        cv2.line(frameCamara, (int((320-b2)/m2),320), (int((480-b2)/m2),480), (255,0,0), 3)
 
         frameCamara[320:480,:,2] = frameCamara[320:640,:,2] + 60
 
         
         cv2.imshow('imagen', frameCamara)
         # cv2.imshow('original', frameCamara)
-        key = cv2.waitKey(1000)
+        key = cv2.waitKey(10000)
         if key == ord('q') or key == ord('Q'):
             break
         if key == ord('s') or key == ord('S'):
